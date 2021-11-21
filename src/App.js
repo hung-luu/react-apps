a026ff4e440aff90cb7901318bb942a29c7860a0
Hung Luu <h.luu1998@gmail.com>
2021-11-21 12:43:17 -0800

    + Added Expenses.js & Expenses.css

--- src/App.js
+++ src/App.js
@@ -1,52 +1,35 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
     {
       title: "iPhone 13 Pro",
       amount: 999.99,
-      date: new Date(2021, 10, 7),
+      date: new Date(2021, 9, 7),
     },
     {
       title: "Macbook Pro",
       amount: 1999.99,
-      date: new Date(2021, 10, 25),
+      date: new Date(2021, 9, 25),
     },
     {
       title: "Carhartt Work Pants",
       amount: 44.99,
-      date: new Date(2021, 10, 20),
+      date: new Date(2021, 9, 27),
     },
     {
       title: "McDonald's Spicy Chicken Sandwich",
       amount: 5.99,
-      date: new Date(2021, 11, 3),
+      date: new Date(2021, 10, 3),
     },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses
+        expenses={expenses}
+      />
     </div>
   );
 }
