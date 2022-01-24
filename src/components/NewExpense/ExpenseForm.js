df9ca73706e7b2d3167f18fc5681417b59b89b6f
Hung Luu <h.luu1998@gmail.com>
2022-01-23 21:24:40 -0800

    + Added bar charts to visualize total amount spent for each month

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -64,7 +64,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
